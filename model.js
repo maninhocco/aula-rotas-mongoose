824cccf91993634331229b0c74b516de05fccea7
Maninho <maninhocco@yahoo.com.br>
2015-10-24 11:38:58 -0200

    Mongoose

--- model.js
+++ model.js
@@ -6,15 +6,12 @@ var db = mongoose.connection;
 db.on('error', function(err) {
   console.log('Erro de conexao', err);
 });
-
 db.on('open', function() {
   console.log('Conex aberta');
 });
-
 db.on('connected', function(err) {
   console.log('Conectado');
 });
-
 db.on('disconnected', function(err) {
   console.log('Desconectado');
 });
